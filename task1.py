4dcd3e93ae70a1e8e7f8d5555b492dc07415c9d5
Konstantin Rybakov <konstantin.rybakov@nmi.com>
2025-03-15 15:52:02 +0200

    added type hinting and logger

--- task1.py
+++ task1.py
@@ -1,56 +1,60 @@
 from abc import ABC, abstractmethod
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(message)s")
 
 
 # Абстрактний клас Vehicle
 class Vehicle(ABC):
-    def __init__(self, make, model, spec):
+    def __init__(self, make: str, model: str, spec: str) -> None:
         self.make = make
         self.model = model
         self.spec = spec
 
     @abstractmethod
-    def start_engine(self):
+    def start_engine(self) -> None:
         pass
 
 
 # Клас Car, що наслідує Vehicle
 class Car(Vehicle):
-    def start_engine(self):
-        print(f"{self.make} {self.model} ({self.spec} Spec): Двигун запущено")
+    def start_engine(self) -> None:
+        logging.info(f"{self.make} {self.model} ({self.spec} Spec): Двигун запущено")
 
 
 # Клас Motorcycle, що наслідує Vehicle
 class Motorcycle(Vehicle):
-    def start_engine(self):
-        print(f"{self.make} {self.model} ({self.spec} Spec): Мотор заведено")
+    def start_engine(self) -> None:
+        logging.info(f"{self.make} {self.model} ({self.spec} Spec): Мотор заведено")
 
 
 # Абстрактна фабрика VehicleFactory
 class VehicleFactory(ABC):
     @abstractmethod
-    def create_car(self, make, model):
+    def create_car(self, make: str, model: str) -> Car:
         pass
 
     @abstractmethod
-    def create_motorcycle(self, make, model):
+    def create_motorcycle(self, make, model) -> Motorcycle:
         pass
 
 
 # Фабрика для транспортних засобів у США
 class USVehicleFactory(VehicleFactory):
-    def create_car(self, make, model):
+    def create_car(self, make: str, model: str) -> Car:
         return Car(make, model, "US")
 
-    def create_motorcycle(self, make, model):
+    def create_motorcycle(self, make: str, model: str) -> Motorcycle:
         return Motorcycle(make, model, "US")
 
 
 # Фабрика для транспортних засобів у ЄС
 class EUVehicleFactory(VehicleFactory):
-    def create_car(self, make, model):
+
+    def create_car(self, make: str, model: str) -> Car:
         return Car(make, model, "EU")
 
-    def create_motorcycle(self, make, model):
+    def create_motorcycle(self, make: str, model: str) -> Motorcycle:
         return Motorcycle(make, model, "EU")
 
 
--- task2.py
+++ task2.py
@@ -1,29 +1,33 @@
 from abc import ABC, abstractmethod
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(message)s")
 
 
 # Клас Book відповідає за зберігання інформації про книгу
 class Book:
-    def __init__(self, title, author, year):
+
+    def __init__(self, title: str, author: str, year: int):
         self.title = title
         self.author = author
         self.year = year
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Title: {self.title}, Author: {self.author}, Year: {self.year}"
 
 
 # Інтерфейс бібліотеки для забезпечення ISP та LSP
 class LibraryInterface(ABC):
     @abstractmethod
-    def add_book(self, book: Book):
+    def add_book(self, book: Book) -> None:
         pass
 
     @abstractmethod
-    def remove_book(self, title: str):
+    def remove_book(self, title: str) -> None:
         pass
 
     @abstractmethod
-    def show_books(self):
+    def show_books(self) -> None:
         pass
 
 
@@ -32,18 +36,18 @@ class Library(LibraryInterface):
     def __init__(self):
         self.books = []
 
-    def add_book(self, book: Book):
+    def add_book(self, book: Book) -> None:
         self.books.append(book)
 
-    def remove_book(self, title: str):
+    def remove_book(self, title: str) -> None:
         self.books = [book for book in self.books if book.title != title]
 
-    def show_books(self):
+    def show_books(self) -> None:
         if not self.books:
-            print("Library is empty.")
+            logging.info("Library is empty.")
         else:
             for book in self.books:
-                print(book)
+                logging.info(book)
 
 
 # Клас LibraryManager для забезпечення DIP
@@ -51,19 +55,19 @@ class LibraryManager:
     def __init__(self, library: LibraryInterface):
         self.library = library
 
-    def add_book(self, title, author, year):
+    def add_book(self, title: str, author: str, year: int) -> None:
         book = Book(title, author, year)
         self.library.add_book(book)
 
-    def remove_book(self, title):
+    def remove_book(self, title: str) -> None:
         self.library.remove_book(title)
 
-    def show_books(self):
+    def show_books(self) -> None:
         self.library.show_books()
 
 
 # Основна функція для керування програмою
-def main():
+def main() -> None:
     library = Library()
     manager = LibraryManager(library)
 
@@ -84,7 +88,7 @@ def main():
             case "exit":
                 break
             case _:
-                print("Invalid command. Please try again.")
+                logging.info("Invalid command. Please try again.")
 
 
 if __name__ == "__main__":
